a3067a8aab5e6001b746dfec61ebb867a98c786b
Kris Warner <kdwarn@protonmail.com>
2023-03-08 13:57:13 -0500

    Add static settings for debug/dev

--- web/urls.py
+++ web/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -14,3 +16,7 @@ urlpatterns = [
     path("resources/partner-orgs", views.partner_orgs, name="partner_orgs"),
     path("search", views.search, name="search"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
